f1ca954ef4c4fa953e11bf93c96855a4e835d4d4
un4ckn0wl3z <slickerth@gmail.com>
2020-01-15 15:55:52 +0700

    finish working with Redis

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-const { clearHash } = require('../services/cache');
 const Blog = mongoose.model('Blog');
+const cleanCache = require('../middlewares/cleanCache');
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
@@ -21,7 +21,7 @@ module.exports = app => {
     res.send(blogs);
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
@@ -37,6 +37,5 @@ module.exports = app => {
       res.send(400, err);
     }
 
-    clearHash(req.user.id);
   });
 };
