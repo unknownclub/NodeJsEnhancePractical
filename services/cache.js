d43ef7f6aa889861d79140ebf58bc724c703dde1
un4ckn0wl3z <slickerth@gmail.com>
2020-01-15 13:48:57 +0700

    add cache monkey patch debug

--- services/cache.js
+++ services/cache.js
@@ -4,6 +4,9 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
     console.log('IM ABOUT To RUN A QUERY');
+
+    console.log(this.getQuery());
+    console.log(this.mongooseCollection.name);
     
     return exec.apply(this, arguments);
 }
